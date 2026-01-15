bbe562b83ac54ab13d5de253fe902b74e6a65fbd
Rohit <rohitmohan2005@gmail.com>
2026-01-16 00:26:22 +0530

    Add rule-based pass/fail logic using if-else

---
+++ notebooks/rule_based_prediction.py
@@ -0,0 +1,7 @@
+marks = int(input("Enter marks: "))
+attendance = int(input("Enter attendance: "))
+if marks>=40 and attendance>75=:
+    print("Prediction: Pass")
+
+else:
+    print("Prediction: Fail")
